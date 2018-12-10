1ea943fd6c49a9c52b6ea97b4f61288abadc1061
oykenfurkan <oykenfurkan@gmail.com>
2018-12-10 03:37:41 +0300

    Create tr.ts

---
+++ src/locale/tr.ts
@@ -0,0 +1,17 @@
+export default {
+    commands: {
+        newNote: {
+            prompt: `Not test`,
+            placeholder: `(Eklemek istediğiniz notunuzu buraya giriniz)`,
+            tooltip: `Not Ekle`
+        },
+        deleteNote: {
+            tooltip: `Bu Notu Sil`,
+            prompt: `Bu notu silmek istediğinizden emin misiniz?`
+        }
+    },
+    common: {
+        yes: `Evet`,
+        no: `Hayır`
+    }
+};
