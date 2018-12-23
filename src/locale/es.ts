d19f318cb11ab8af91083ace4bc5c2358c45d316
chocomochi <46060628+chocomochi@users.noreply.github.com>
2018-12-23 21:13:54 +0100

    Spanish translation
    Quick translation, thanks for instructions.

---
+++ src/locale/es.ts
@@ -0,0 +1,17 @@
+export default {
+    commands: {
+        newNote: {
+            prompt: `Nota de prueba`,
+            placeholder: `(Escribe aquí tu nota)`,
+            tooltip: `Añadir nota`
+        },
+        deleteNote: {
+            tooltip: `Borrar nota`,
+            prompt: `¿Quiere borrar esta nota?`
+        }
+    },
+    common: {
+        yes: `sí`,
+        no: `no`
+    }
+};
