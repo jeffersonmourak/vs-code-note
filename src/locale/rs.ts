1a4739fecbf4a57f6c3c8c1aa15608a3a271df26
l3xcore <46606386+l3xcore@users.noreply.github.com>
2019-01-13 21:30:38 +0300

    Create rs.ts

---
+++ src/locale/rs.ts
@@ -0,0 +1,17 @@
+export default {
+    commands: {
+        newNote: {
+            prompt: `Тестовая заметка`,
+            placeholder: `(Напечатайте свою заметку здесь)`,
+            tooltip: `Добавить новую заметку`
+        },
+        deleteNote: {
+            tooltip: `Удалить эту заметку`,
+            prompt: `Вы действительно хотите удалить эту заметку?`
+        }
+    },
+    common: {
+        yes: `да`,
+        no: `нет`
+    }
+};
